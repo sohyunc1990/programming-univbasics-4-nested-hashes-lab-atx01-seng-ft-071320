c69ea98e73fa7ee421baeb9d391456a787d92815
Sohyun Lee <sohyunc1990@gmail.com>
2020-07-04 21:41:53 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,8 @@ def hopper
         :languages => ["C"]
       }
     }
-
+    
+  programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -36,6 +37,8 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
+    
+    programmer_hash[:alan_kay][:known_for]
 
 end
 
@@ -56,6 +59,8 @@ def dennis_ritchies_language
       }
     }
 
+programmer_hash[:dennis_ritchie][:languages][0]
+
 end
 
 def adding_matz
@@ -68,6 +73,10 @@ def adding_matz
 
 	programmer_hash = 
  		{
+ 		  :yukihiro_matsumoto => {
+        :known_for => [],
+        :languages => []
+      },
       :grace_hopper => {
         :known_for => "COBOL",
         :languages => ["COBOL", "FORTRAN"]
@@ -80,7 +89,12 @@ def adding_matz
         :known_for => "Unix",
         :languages => ["C"]
       }
-    }
+   }
+    
+    programmer_hash[:yukihiro_matsumoto][:known_for] << "Ruby"
+    programmer_hash[:yukihiro_matsumoto][:languages] << ["LISP", "C"]
+    
+  programmer_hash
 
 end
 
@@ -103,6 +117,10 @@ def changing_alan
         :languages => ["C"]
       }
     }
+    
+  programmer_hash[:alan_kay][:known_for] = "GUI"
+  
+  programmer_hash
 
 end
 
@@ -126,4 +144,7 @@ def adding_to_dennis
       }
     }
 
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  
+  programmer_hash
 end
